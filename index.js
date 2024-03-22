6cf3a1f65317893ac8618e9742db399c7ae50e71
chitranshu <chitranshuctr@gmail.com>
2024-03-22 22:25:36 +0530

    done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const port = 3002;
 const axios = require("axios");
 const app = express();
-const sha256= require('sha256')
+const sha256 = require("sha256");
 
 const uniqid = require("uniqid");
 
@@ -36,15 +36,15 @@ app.get("/pay", (req, res) => {
 
   const bufferObj = Buffer.from(JSON.stringify(payload, "utf-8"));
   const base64encoded = bufferObj.toString("base64");
-  const xVerify = sha256(base64encoded + payEndpoint + SALT_KEY) + "###" + SALT_INDEX
+  const xVerify =sha256(base64encoded + payEndpoint + SALT_KEY) + "###" + SALT_INDEX;
 
   const options = {
     method: "post",
-    url: `${PHONE_PE_HOST_URL}/pg/v1/pay`,
+    url: `${PHONE_PE_HOST_URL}${payEndpoint}`,
     headers: {
-      accept: "text/plain",
+      accept: "application/json",
       "Content-Type": "application/json",
-      "X-VERIFY": xVerify
+      "X-VERIFY": xVerify,
     },
     data: {
       request: base64encoded,
@@ -55,9 +55,14 @@ app.get("/pay", (req, res) => {
     .then(function (response) {
       console.log(response.data);
 
-      res.send(response.data)
+      res.send(response.data);
     })
     .catch(function (error) {
       console.error(error);
     });
 });
+
+
+app.listen(port, ()=>{
+  console.log("Chitranshu.....")
+})
